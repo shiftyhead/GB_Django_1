5982f112133bae39489ec28d0ef3392e0014f1f4
shiftyhead <spirin.md@gmail.com>
2018-09-29 18:09:47 +0400

    добавление моделей

--- server/products/views.py
+++ server/products/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
-import json
+# import json
+from .models import ProductCategory, Product
 
 def product_list(request):
 
-    with open('products/static/products/json/items.json') as json_file:
-        context = json.load(json_file)
-    context = {'items':[i['title'] for i in context['items']]}
+    # with open('products/static/products/json/items.json') as json_file:
+    #     context = json.load(json_file)
+    # context = {'items':[i['title'] for i in context['items']]}
+
+    context = {'items': Product.objects.all()}
 
     return render(request, 'products/list.html', context)
 
 def product_detail(request, pk):
 
-    with open('products/static/products/json/items.json') as json_file:
-        context = json.load(json_file)
+    # with open('products/static/products/json/items.json') as json_file:
+        # context = json.load(json_file)
 
-    return render(request, 'products/detail.html', {'product_title': context['items'][pk]['title'],
-                                                    'product_text': context['items'][pk]['text']})
+    context = Product.objects.get(id=pk)
+
+    return render(request, 'products/detail.html', {'product_title': context.name,
+                                                    'product_text': context.snippet})
