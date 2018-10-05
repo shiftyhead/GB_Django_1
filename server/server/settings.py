6dd6ec0145aa5dfb60b6ad268456952878eb978a
shiftyhead <spirin.md@gmail.com>
2018-10-05 10:31:09 +0400

    add load to DB via products/management/commands/fill_db.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -133,5 +133,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'server', 'static'),
+    # os.path.join(BASE_DIR, 'server', 'static'),
 ]
