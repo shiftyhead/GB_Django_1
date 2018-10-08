e0ba03f132fcfcd230fa143330ed85490f43c08a
shiftyhead <spirin.md@gmail.com>
2018-10-08 14:44:19 +0400

    добавлено меню категорий

--- server/products/urls.py
+++ server/products/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'products'
 
 urlpatterns = [
     path('<int:pk>/', views.product_detail, name='detail'),
+    re_path(r'^category/(?P<pk>\d+)/$', views.product_list, name='category'),
     path('', views.product_list, name='list'),
 ]
--- server/products/views.py
+++ server/products/views.py
@@ -2,15 +2,28 @@ from django.shortcuts import render, get_object_or_404
 # import json
 from .models import ProductCategory, Product
 
-def product_list(request):
+def product_list(request, pk=None):
+    title = 'продукты'
+    links_menu = ProductCategory.objects.all()
+    if pk:
+        category = ProductCategory.objects.filter(id=pk).order_by('name')
+        products = Product.objects.filter(category__id=pk).order_by('name')
+        context = {
+            'title': title,
+            'links_menu': links_menu,
+            'categories': category,
+            'items': products
+        }
 
-    # with open('products/static/products/json/items.json') as json_file:
-    #     context = json.load(json_file)
-    # context = {'items':[i['title'] for i in context['items']]}
-
-    context = {'items': Product.objects.all()}
-
+    else:
+        products = Product.objects.all().order_by('name')
+        context = {
+            'title': title,
+            'links_menu': links_menu,
+            'items': products
+        }
     return render(request, 'products/list.html', context)
+
 
 def product_detail(request, pk):
 
