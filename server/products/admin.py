6c878e54754edcd76cbc9d318b17d225131f6787
shiftyhead <spirin.md@gmail.com>
2018-10-13 18:25:56 +0400

    добавление настроек админ-панели для Product и ProductCategory

--- server/products/admin.py
+++ server/products/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Product)
-admin.site.register(models.ProductCategory)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'cost', 'category']
+    list_filter = ['category']
+    search_fields = ['name', 'snippet']
+
+
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name', 'description']
+
+
+admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.ProductCategory, ProductCategoryAdmin)
