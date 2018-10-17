1e0462f9b721428719639ecb92e112d63977f956
shiftyhead <spirin.md@gmail.com>
2018-10-17 16:48:31 +0400

    добавление CBV LoginView

--- server/authapp/urls.py
+++ server/authapp/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, re_path
 import authapp.views as authapp
-
+from .views import LoginView
 app_name = 'auth'
 
 urlpatterns = [
-    re_path(r'^login/$', authapp.login, name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     re_path(r'^logout/$', authapp.logout, name='logout'),
     re_path(r'^register/$', authapp.register, name='register'),
     re_path(r'^edit/$', authapp.edit, name='edit'),
